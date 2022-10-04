99fa6f6d13aaa6f88783d6a2bcf35e8a35e6976f
Prachi Kashyap <prachikashyap@Prachis-MacBook-Air.local>
2022-10-04 12:32:07 -0700

    Homework 1
    Fizzbuzz Program - First Assignment

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -1,5 +1,14 @@
+//Description : Program to print Fizz if a number is divisible by 3, Buzz if a number
+//is divisible by 5 and Fizz Buzz if it is divided by 3 and 5 both
+
 const fizzbuzz = () => {
-  // Add your code here
+  for (let number = 1; number <= 100; number++) {
+    let wordToPrint = number;
+    if (number % 3 === 0 && number % 5 === 0) wordToPrint = "Fizz Buzz";
+    else if (number % 3 === 0) wordToPrint = "Fizz";
+    else if (number % 5 === 0) wordToPrint = "Buzz";
+    console.log(wordToPrint);
+  }
 };
 
 fizzbuzz();
