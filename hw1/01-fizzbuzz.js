581af4b1c861a547551882275c40fdbab5b79efb
Prachi Kashyap <prachikashyap@Prachis-MacBook-Air.local>
2022-10-05 11:11:47 -0700

    Homework 1 - Updated
    Completed the prettier settings and recommitted

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -2,16 +2,16 @@
 //is divisible by 5 and Fizz Buzz if it is divided by 3 and 5 both
 
 const fizzbuzz = () => {
-  for (let number = 1; number <= 100; number++) {
-    let wordToPrint = number;
-    if (number % 3 === 0 && number % 5 === 0) wordToPrint = "Fizz Buzz";
-    else if (number % 3 === 0) wordToPrint = "Fizz";
-    else if (number % 5 === 0) wordToPrint = "Buzz";
-    console.log(wordToPrint);
-  }
-};
+    for (let number = 1; number <= 100; number += 1) {
+        let wordToPrint = number
+        if (number % 3 === 0 && number % 5 === 0) wordToPrint = 'Fizz Buzz'
+        else if (number % 3 === 0) wordToPrint = 'Fizz'
+        else if (number % 5 === 0) wordToPrint = 'Buzz'
+        console.log(wordToPrint)
+    }
+}
 
-fizzbuzz();
+fizzbuzz()
 // 1
 // 2
 // fizz
