0ed293308453b2db7100fe4a004c6fd23d320757
Prachi Kashyap <pk23@pdx.edu>
2022-10-18 16:04:47 -0700

    Homework 2 Commit
    Code changes completed for HW 2

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,3 +1,19 @@
-document.querySelector('#reverse').onclick = () => {
-  // Add your code here
-};
+function reverse() {
+    let number = document.getElementById('input').value;
+    let originalNumber = number;
+    if (number < 10000000 || number > 99999999) {
+        document.getElementById('reversedNumber').innerHTML =
+            'Error: Please input an 8 digit number!';
+        document.getElementById('reversedNumber').style.color = '#aa3333';
+    } else {
+        let remainder = 0;
+        let reverse = 0;
+        while (number != 0) {
+            remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = Math.floor(number / 10);
+        }
+        document.getElementById('reversedNumber').innerHTML = originalNumber + ' --> ' + reverse;
+        document.getElementById('reversedNumber').style.color = '#338833';
+    }
+}
--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,3 +1,18 @@
 function handleSubmit(event) {
-  // Add your code here
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let feedback = document.getElementById('feedback').value;
+    let checkVal = document.getElementById('checkVal').checked;
+    let checkBoxResp = '';
+    if (checkVal === true) checkBoxResp = 'Yes, I would like to join the newsletter.';
+    else checkBoxResp = 'No, thank you.';
+    if (name === '' || email === '') alert('Please fill the required fields');
+    else {
+        console.group('======== Form Submission ========');
+        console.log('Name: ' + name);
+        console.log('Email: ' + email);
+        console.log('Feedback: ' + feedback);
+        console.log('Newsletter: ' + checkBoxResp);
+        console.groupEnd();
+    }
 }
