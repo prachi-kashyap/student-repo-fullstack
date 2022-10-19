aca6f89b162a642a47e389b83153825c64eb4a25
Prachi Kashyap <pk23@pdx.edu>
2022-10-18 22:15:29 -0700

    Homework 2 Updated
    Updated Reverse.js file

--- hw2/02-reverse.js
+++ hw2/02-reverse.js
@@ -1,19 +1,12 @@
 function reverse() {
     let number = document.getElementById('input').value;
-    let originalNumber = number;
-    if (number < 10000000 || number > 99999999) {
+    if (number.toString().length != 8) {
         document.getElementById('reversedNumber').innerHTML =
             'Error: Please input an 8 digit number!';
         document.getElementById('reversedNumber').style.color = '#aa3333';
     } else {
-        let remainder = 0;
-        let reverse = 0;
-        while (number != 0) {
-            remainder = number % 10;
-            reverse = reverse * 10 + remainder;
-            number = Math.floor(number / 10);
-        }
-        document.getElementById('reversedNumber').innerHTML = originalNumber + ' --> ' + reverse;
+        let reverse = number.split('').reverse().join('');
+        document.getElementById('reversedNumber').innerHTML = number + ' --> ' + reverse;
         document.getElementById('reversedNumber').style.color = '#338833';
     }
 }
